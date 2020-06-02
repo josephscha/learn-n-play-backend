ae64ec0cbc9501fc48b8036780781bd815da80df
Josephscha <Josephshincha@gmail.com>
2020-06-02 17:12:49 -0400

    relationship setup

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,8 @@
 class Course < ApplicationRecord
+    has_many :user_courses
+    has_many :users, through: :user_courses
+    has_many :course_problems, as: :problemable
+    has_many :reading_problems, through: :course_problems, source: :problemable, source_type: "ReadingProblem"
+    has_many :spelling_problems, through: :course_problems, source: :problemable, source_type: "SpellingProblem"
+    has_many :math_problems, through: :course_problems, source: :problemable, source_type: "MathProblem"
 end
--- app/models/math_problem.rb
+++ app/models/math_problem.rb
@@ -1,2 +1,4 @@
 class MathProblem < ApplicationRecord
+    has_many :course_problems, as: :problemable
+    has_many :courses, through: :course_problems, source: :problemable, source_type: "Course"
 end
