93bedf6aebf1f4cac16c10c68c831bddaa92d3cf
Josephscha <Josephshincha@gmail.com>
2020-06-02 17:24:44 -0400

    Relationship test, initial seed file commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Course.destroy_all
+UserCourse.destroy_all
+MathProblem.destroy_all
+SpellingProblem.destroy_all
+ReadingProblem.destroy_all
+CourseProblem.destroy_all
+
+User.create(username: "Joseph")
+Course.create(difficulty: "Easy", title: "Level 1 reading", description: "First steps to reading")
+UserCourse.create(user: User.first, course: Course.first)
+
+MathProblem.create(problem: "1 + 1", answer: "2")
+ReadingProblem.create(problem: "apple", answer: "apple")
+SpellingProblem.create(problem: "a _ _ _ e", answer: "apple")
+
+CourseProblem.create(course_id: Course.first, problemable_type: "ReadingProblem", problemable_id: ReadingProblem.first.id)
+
+puts "Seeding complete"
